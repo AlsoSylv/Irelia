25fcae6db37a027ca56561f7f55b7a914582e110
Cynthia <64978191+AlsoSylv@users.noreply.github.com>
2023-01-22 20:27:00 -0500

    Fix bug with using include_bytes as a constant

--- src/utils/request.rs
+++ src/utils/request.rs
@@ -3,10 +3,8 @@ use hyper::Client;
 use hyper_tls::HttpsConnector;
 use tokio_native_tls::native_tls::{Certificate, TlsConnector};
 
-const PEM: &[u8; 1516] = include_bytes!("../riotgames.pem");
-
 pub(crate) fn setup_hyper_client() -> Result<Client<HttpsConnector<HttpConnector>>, ()> {
-    let cert = Certificate::from_pem(PEM).unwrap();
+    let cert = Certificate::from_pem(include_bytes!("../riotgames.pem")).unwrap();
     let tls = TlsConnector::builder()
         .add_root_certificate(cert)
         .build()
