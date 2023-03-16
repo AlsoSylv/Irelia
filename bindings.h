7f2e0d12ed0d368a963395ba5dfa9232bb810fa5
AlsoSylv <64978191+AlsoSylv@users.noreply.github.com>
2023-03-16 13:26:08 -0400

    LCU C API

--- bindings.h
+++ bindings.h
@@ -4,47 +4,32 @@
 #include <ostream>
 #include <new>
 
-/// Custom errors for the LCU
-enum class Error {
-  /// Expected or input type are incorrect
-  FailedParseJson = 0,
-  /// The LCU stopped running
-  LCUStoppedRunning = 1,
-  /// The game stopped running
-  LeagueStoppedRunning = 2,
-  /// The following request is invalid
-  InvalidRequest = 3,
-  /// The request body is invalid
-  InvalidBody = 4,
-  /// The LCU was never running
-  LCUProcessNotRunning = 5,
-  /// Could not locate port for the LCU
-  PortNotFound = 6,
-  /// The sub process could not be spawned
-  CannotLaunchTerminal = 7,
-  /// Auth token for the LCU could not be found
-  AuthTokenNotFound = 8,
+struct LCU;
+
+struct NewLCU {
+  LCU *client;
+  int error;
 };
 
-struct LCUC;
-
-template<typename T = void, typename E = void>
-struct Result;
+struct LcuResponse {
+  const char *json;
+  int error;
+};
 
 extern "C" {
 
-Result<LCUC, Error> *lcu_new();
+NewLCU lcu_new();
 
-const Result<const char*, Error> *lcu_get(LCUC *client, char *endpoint);
+LcuResponse lcu_get(LCU *client, const char *endpoint);
 
-const Result<const char*, Error> *lcu_post(LCUC *client, char *endpoint, char *body);
+LcuResponse lcu_post(LCU *client, const char *endpoint, const char *body);
 
-const Result<const char*, Error> *lcu_put(LCUC *client, char *endpoint, char *body);
+LcuResponse lcu_put(LCU *client, const char *endpoint, const char *body);
 
-const Result<const char*, Error> *lcu_delete(LCUC *client, char *endpoint);
+LcuResponse lcu_delete(LCU *client, const char *endpoint);
 
-const Result<const char*, Error> *lcu_head(LCUC *client, char *endpoint);
+LcuResponse lcu_head(LCU *client, const char *endpoint);
 
-void lcu_drop(LCUC *client);
+void lcu_drop(LCU *client);
 
 } // extern "C"
--- src/lib.rs
+++ src/lib.rs
@@ -23,77 +23,104 @@ pub mod ws;
 /// Custom errors for the LCU
 pub enum Error {
     /// Expected or input type are incorrect
-    FailedParseJson = 0,
+    FailedParseJson = 10,
     /// The LCU stopped running
-    LCUStoppedRunning = 1,
+    LCUStoppedRunning = 11,
     #[cfg(feature = "in_game")]
     /// The game stopped running
-    LeagueStoppedRunning = 2,
+    LeagueStoppedRunning = 12,
     /// The following request is invalid
-    InvalidRequest = 3,
+    InvalidRequest = 13,
     /// The request body is invalid
-    InvalidBody = 4,
+    InvalidBody = 14,
     /// The LCU was never running
-    LCUProcessNotRunning = 5,
+    LCUProcessNotRunning = 15,
     /// Could not locate port for the LCU
-    PortNotFound = 6,
+    PortNotFound = 16,
     /// The sub process could not be spawned
-    CannotLaunchTerminal = 7,
+    CannotLaunchTerminal = 17,
     /// Auth token for the LCU could not be found
-    AuthTokenNotFound = 8,
+    AuthTokenNotFound = 18,
 }
 
 #[cfg(feature = "c")]
 mod c_lcu {
-    use std::ffi::{c_char, CStr, CString};
+    use std::ffi::{c_char, c_int, CStr, CString};
 
     use futures_util::Future;
     use tokio::runtime::Runtime;
 
     use crate::{rest::LCUClient, Error};
 
-    pub struct LCUC<'a> {
+    #[repr(C)]
+    pub struct LcuResponse {
+        pub json: *const c_char,
+        pub error: c_int,
+    }
+
+    #[repr(C)]
+    pub struct NewLCU<'a> {
+        pub client: *mut LCU<'a>,
+        pub error: c_int,
+    }
+
+    pub struct LCU<'a> {
         client: LCUClient<'a>,
         rt: Runtime,
     }
 
-    impl LCUC<'_> {
-        fn new<'a>() -> Result<LCUC<'a>, Error> {
-            let client = LCUClient::new()?;
+    impl LCU<'_> {
+        fn new<'a>() -> NewLCU<'a> {
+            let client = LCUClient::new();
             let rt = tokio::runtime::Builder::new_current_thread()
                 .enable_all()
                 .build()
                 .expect("Pain");
 
-            Ok(LCUC { client, rt })
+            match client {
+                Ok(client) => NewLCU {
+                    client: Box::into_raw(Box::new(LCU { client, rt })),
+                    error: 0,
+                },
+                Err(err) => NewLCU {
+                    client: std::ptr::null_mut(),
+                    error: err as c_int,
+                },
+            }
         }
     }
 
     fn lcu_generic(
         fut: impl Future<Output = Result<Option<CString>, Error>>,
         rt: &Runtime,
-    ) -> *const Result<*const c_char, Error> {
+    ) -> LcuResponse {
         let result = rt.block_on(fut);
 
         match result {
             Ok(result) => match result {
-                Some(c_string) => &Ok(c_string.as_ptr()),
-                None => &Ok(std::ptr::null()),
+                Some(c_string) => LcuResponse {
+                    json: c_string.as_ptr(),
+                    error: 0,
+                },
+                None => LcuResponse {
+                    json: std::ptr::null(),
+                    error: 0,
+                },
             },
-            Err(err) => &Err(err),
+            Err(err) => LcuResponse {
+                json: std::ptr::null(),
+                error: err as c_int,
+            },
         }
     }
 
     #[no_mangle]
-    pub extern "C" fn lcu_new<'a>() -> *mut Result<LCUC<'a>, Error> {
-        Box::into_raw(Box::new(LCUC::new()))
+    pub extern "C" fn lcu_new<'a>() -> NewLCU<'a> {
+        LCU::new()
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn lcu_get<'a>(
-        client: *mut LCUC,
-        endpoint: *mut c_char,
-    ) -> *const Result<*const c_char, Error> {
+    pub unsafe extern "C" fn lcu_get<'a>(client: *mut LCU, endpoint: *const c_char) -> LcuResponse {
         let client = &*client;
         let endpoint = CStr::from_ptr(endpoint).to_string_lossy();
         let fut = client.client.get::<CString>(&endpoint);
@@ -102,10 +129,10 @@ mod c_lcu {
 
     #[no_mangle]
     pub unsafe extern "C" fn lcu_post<'a>(
-        client: *mut LCUC,
-        endpoint: *mut c_char,
-        body: *mut c_char,
-    ) -> *const Result<*const c_char, Error> {
+        client: *mut LCU,
+        endpoint: *const c_char,
+        body: *const c_char,
+    ) -> LcuResponse {
         let client = &*client;
         let endpoint = CStr::from_ptr(endpoint).to_string_lossy();
         let body = CStr::from_ptr(body).to_string_lossy();
@@ -115,10 +142,10 @@ mod c_lcu {
 
     #[no_mangle]
     pub unsafe extern "C" fn lcu_put<'a>(
-        client: *mut LCUC,
-        endpoint: *mut c_char,
-        body: *mut c_char,
-    ) -> *const Result<*const c_char, Error> {
+        client: *mut LCU,
+        endpoint: *const c_char,
+        body: *const c_char,
+    ) -> LcuResponse {
         let client = &*client;
         let endpoint = CStr::from_ptr(endpoint).to_string_lossy();
         let body = CStr::from_ptr(body).to_string_lossy();
@@ -127,10 +154,7 @@ mod c_lcu {
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn lcu_delete<'a>(
-        client: *mut LCUC,
-        endpoint: *mut c_char,
-    ) -> *const Result<*const c_char, Error> {
+    pub unsafe extern "C" fn lcu_delete<'a>(client: *mut LCU, endpoint: *const c_char) -> LcuResponse {
         let client = &*client;
         let endpoint = CStr::from_ptr(endpoint).to_string_lossy();
         let fut = client.client.delete::<CString>(&endpoint);
@@ -138,10 +162,7 @@ mod c_lcu {
     }
 
     #[no_mangle]
-    pub unsafe extern "C" fn lcu_head<'a>(
-        client: *mut LCUC,
-        endpoint: *mut c_char,
-    ) -> *const Result<*const c_char, Error> {
+    pub unsafe extern "C" fn lcu_head<'a>(client: *mut LCU, endpoint: *const c_char) -> LcuResponse {
         let client = &*client;
         let endpoint = CStr::from_ptr(endpoint).to_string_lossy();
         let fut = client.client.head::<CString>(&endpoint);
@@ -149,7 +170,7 @@ mod c_lcu {
     }
 
     #[no_mangle]
-    pub extern "C" fn lcu_drop<'a>(client: *mut LCUC) {
+    pub extern "C" fn lcu_drop<'a>(client: *mut LCU) {
         let client = unsafe { Box::from_raw(client) };
         drop(client)
     }
