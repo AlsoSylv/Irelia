14530c49c43beeaa9f1d74bd2fdc23bb2fc16b7b
Cynthia <64978191+AlsoSylv@users.noreply.github.com>
2023-03-27 22:23:30 -0400

    New encoder

--- src/utils/encoder.rs
+++ src/utils/encoder.rs
@@ -2,51 +2,48 @@
 //! It goes into detail about the entire thing, and why it works the way it does, and I hightly reccomend reading it
 //! Very big thanks to Tiemen for writing it!
 
-const UPPERCASEOFFSET: u8 = 65;
-const LOWERCASEOFFSET: u8 = 71;
-const DIGITOFFSET: u8 = 4;
+const BASE64_ALPHABET: [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9', '+', '/',
+];
 
-fn split(chunk: &[u8]) -> Vec<u8> {
-    match chunk.len() {
-        1 => vec![&chunk[0] >> 2, (&chunk[0] & 0b00000011) << 4],
-
-        2 => vec![
-            &chunk[0] >> 2,
-            (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4,
-            (&chunk[1] & 0b00001111) << 2,
-        ],
-
-        3 => vec![
-            &chunk[0] >> 2,
-            (&chunk[0] & 0b00000011) << 4 | &chunk[1] >> 4,
-            (&chunk[1] & 0b00001111) << 2 | &chunk[2] >> 6,
-            &chunk[2] & 0b00111111,
-        ],
-
-        _ => unreachable!(),
-    }
-}
-
-fn encode_chunk(chunk: Vec<u8>) -> [char; 4] {
-    let mut out = ['=', '=', '=', '='];
-
-    chunk.iter().enumerate().for_each(|(i, index)| {
-        out[i] = match index {
-            0..=25 => (*index + UPPERCASEOFFSET) as char,
-            26..=51 => (*index + LOWERCASEOFFSET) as char,
-            52..=61 => (*index - DIGITOFFSET) as char,
-            62 => 43 as char,
-            63 => 47 as char,
-
-            _ => unreachable!(),
-        };
-    });
-
-    out
-}
-
-pub(super) fn encode(input: String) -> String {
-    String::from_iter(input.as_bytes().chunks(3).map(split).flat_map(encode_chunk))
+pub(super) fn encode(string: String) -> String {
+    string
+        .as_bytes()
+        .chunks(3)
+        .flat_map(|chunk| {
+            let bit_1 = &chunk[0] >> 2;
+            let bit_2 = (&chunk[0] & 0b00000011) << 4;
+            let char_1 = BASE64_ALPHABET[bit_1 as usize];
+            match chunk.len() {
+                1 => [char_1, BASE64_ALPHABET[bit_2 as usize], '=', '='],
+                2 => {
+                    let bit_2 = bit_2 | &chunk[1] >> 4;
+                    let bit_3 = (&chunk[1] & 0b00001111) << 2;
+                    [
+                        char_1,
+                        BASE64_ALPHABET[bit_2 as usize],
+                        BASE64_ALPHABET[bit_3 as usize],
+                        '=',
+                    ]
+                }
+                3 => {
+                    let bit_2 = bit_2 | &chunk[1] >> 4;
+                    let bit_3 = (&chunk[1] & 0b00001111) << 2 | &chunk[2] >> 6;
+                    let bit_4 = &chunk[2] & 0b00111111;
+                    [
+                        char_1,
+                        BASE64_ALPHABET[bit_2 as usize],
+                        BASE64_ALPHABET[bit_3 as usize],
+                        BASE64_ALPHABET[bit_4 as usize],
+                    ]
+                }
+                _ => unreachable!(),
+            }
+        })
+        .collect()
 }
 
 #[cfg(test)]
