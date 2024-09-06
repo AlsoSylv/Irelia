f14c02280ce40e33fef4146dca3c1a0aacbc2b0d
AlsoSylv <64978191+AlsoSylv@users.noreply.github.com>
2024-09-05 21:31:42 -0400

    Update example

--- irelia/examples/websocket.rs
+++ irelia/examples/websocket.rs
@@ -1,6 +1,5 @@
 use irelia::ws::types::{Event, EventKind};
-use irelia::ws::{Flow, LcuWebSocket, Subscriber};
-use std::ops::ControlFlow;
+use irelia::ws::{LcuWebSocket, Subscriber};
 use std::thread;
 use std::time::Duration;
 
@@ -8,12 +7,10 @@ fn main() {
     struct EventCounter(u32);
 
     impl Subscriber for EventCounter {
-        fn on_event(&mut self, event: &Event) -> ControlFlow<(), Flow> {
+        fn on_event(&mut self, event: &Event, _: &mut bool) {
             println!("{event:?}");
             println!("{}", self.0);
             self.0 += 1;
-
-            ControlFlow::Continue(Flow::Continue)
         }
     }
 
